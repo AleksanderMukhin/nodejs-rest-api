ec50570a9e6e330d298ab97e69475ecf4dd0bbe4
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-08-09 13:20:56 +0200

    init project

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-const app = require('./app')
+const app = require('./app');
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST = "mongodb+srv://Oleksander:150487-mO@cluster0.swrdjil.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose.set('strictQuery', true);
+
+mongoose.connect(DB_HOST)
+  .then(() => console.log("Database connection successful"))
+  .then(()=>app.listen(3000))
+  .catch(error => {
+    console.log(error.message);
+    process.exit(1);
+  })
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
