d5287fdc4051a650007259794e096a32676dc71b
AleksanderMukhin <mukhin.aleksander.ua@gmail.com>
2023-08-15 14:53:50 +0200

    final

--- middellwares/index.js
+++ middellwares/index.js
@@ -1,5 +1,6 @@
 const {validateBody, validateFavorites} = require('./validateBody');
 const isValidId = require('./isValidId');
-const authintecate =  require('./authenticate')
+const authintecate = require('./authenticate');
+const upload = require('./upload');
 
-module.exports = { validateBody, isValidId, validateFavorites, authintecate};
+module.exports = { validateBody, isValidId, validateFavorites, authintecate, upload,};
